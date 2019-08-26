417338eb9cb0e9e01632f7867d4b3cf8d6bd3a57
Iain Moncrief <Iainmoncrief@gmail.com>
2019-08-26 08:10:24 -1000

    Fixed segfault. Works on my Mac.

--- examples/drawables.cpp
+++ examples/drawables.cpp
@@ -176,26 +176,11 @@ int main(int argc, char **argv)
 
     Hierarchy *h = new Hierarchy(argc, argv);
 
-    GameObject *gm1 = new GameObject();
-    gm1->AddComponent(new components::Transform());
-    gm1->AddComponent(new components::RigidBody());
-    gm1->AddComponent(new components::drawables::Circle());
-    gm1->AddComponent(new components::drawables::ForceLines());
-
-    components::Transform *transform = gm1->GetComponent<components::Transform>();
-    transform->position = Vector2D(100, 1);
-    transform->velocity = Vector2D(0, 0);
-    cout << transform->position.magnitude() << endl;
-
-    components::RigidBody *rb = gm1->GetComponent<components::RigidBody>();
-    rb->mass = 100;
-
     GameObject *gm2 = new GameObject();
-    gm2->AddComponent(new Spawner);
+    gm2->AddComponent(new Spawner());
     Spawner *spawner = gm2->GetComponent<Spawner>();
     spawner->SetHierarchyRefference(h);
 
-    //h->Add(gm1);
     h->Add(gm2);
 
     h->RunMainLoop();
